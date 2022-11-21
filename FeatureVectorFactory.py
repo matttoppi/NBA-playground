455110feff663f067a0519f81dded88f14a3c1d1
Lucas Guerrette <lgg1004@wildcats.unh.edu>
2022-11-21 14:44:16 -0500

    Test commit of changes to the FeatureVectorFactory.py file.

--- FeatureVectorFactory.py
+++ FeatureVectorFactory.py
@@ -23,14 +23,11 @@ class RaptorVectorFactory:
     file = ""
     raptor_id_file = ""
 
-
     def _init_(self):
-
         # load the raptor ID data file
         raptorIDs = self.player_id_dict()
         file = "preVectorDATA/RaptorData (2014-2023) - raptor_data14-23.csv"
         raptor_id_file = "preVectorDATA/raptor_player_id_dict - raptor_player_id_dict.csv"
-
 
     def load_raptor_data(file):
         """
@@ -43,14 +40,33 @@ class RaptorVectorFactory:
     name_to_id, id_to_name = player_id_dict()
     super_data = load_raptor_data("preVectorDATA/raptor_data.csv")
 
-
-    def player_id_dict():
+    def player_id_dict(self):
         """
 
         :param file: filename
-        returns dict, dict tuple of player_name->player_id and player_id->player_name respectively
+        :returns dict, dict tuple of player_name->player_id and player_id->player_name respectively
         """
         df = pd.read_csv(raptor_id_file)
         x = dict(df.itertuples(False, None))
         y = {v: k for k, v in x.items()}
         return x, y
+
+
+class TeamDataFeatureFactory:
+    """This class is meant to be used to load in data from our CSV files and create feature vectors out of them."""
+
+    # Global variables for the class ====================================================
+    file = None  # PATH to file containing the team history data
+    dataframe = None  # Variable representing the processed/read-in CSV file
+
+    def __init__(self, filename):
+        """Initialize the factory by providing it with the file to read data from"""
+        file = filename
+        dataframe = pd.read_csv(file)
+
+    def split_by_season(self):
+        """
+        This method takes the data from the CSV file and splits it up into matrices that represent each season
+        :returns A list containing new matrices, with each representing all the team data for a given season/year
+        """
+        
