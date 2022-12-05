7858aad4be3c345e8954b4e2b91bbed9171374e0
Matthew Toppi <47908195+matttoppi@users.noreply.github.com>
2022-12-05 12:57:10 -0500

    push of begining of train models

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,13 @@ I
 .idea/leetcode/editor.xml,4/e/4e1d1083192753a7663628e0017ffffe55ae10ed
 ?
 call_library.py,f/6/f61355b498946da8f3634ef24aa30b80a7460aad
+w
+G.idea/sonarlint/issuestore/4/e/4e1d1083192753a7663628e0017ffffe55ae10ed,e/c/ec5ed7c1d9562c3dd64a6c9732b98c92c16d72c3
+W
+'preVectorDATA/raptor_player_id_dict.csv,e/e/ee9922a3b61590f0054258c791e00a173f1381de
+N
+preVectorDATA/TEAM_ROSTERS.csv,d/c/dc4e8053f678bf0ae6e853d4fed4d39b7857833b
+J
+preVectorDATA/TeamData.csv,2/0/2078821dd2e2fb206584f95cca3020812edd4103
+S
+#preVectorDATA/RPM_Player_Season.csv,0/e/0ef428bee266f54cbc03e0a869854cd6a98febed
--- Training.py
+++ Training.py
@@ -4,6 +4,15 @@ from evalMetric import *
 import numpy as np
 import pandas as pd
 #import xgboost as xgb
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv1D, MaxPooling1D, Flatten
+
+
+from sklearn.preprocessing import MinMaxScaler
+
 
 
 
@@ -88,6 +97,71 @@ def matt_train(train, test):
     print("Matt's Overall Error: ", total_diff)
     print("Matt Avg Error:", error)
     print("-----------------------------\n")
+
+
+
+
+def matts_LSTM_train(train, test):
+    """
+    LSTM model that is trained on the data
+    author: Matt
+    :return:
+    """
+    print("-----------------------------\n"
+          "Matt's LSTM Model:\n")
+
+    learning_rate = 0.01
+    epochs = 100
+    batch_size = 100
+    num_units = 100
+
+    X = torch.FloatTensor(train[0][0]) # input
+    y = torch.FloatTensor(train[0][21]) # 21 is the index of the score column
+
+    X = X.view(-1, 1, 1) # reshape the data to fit the LSTM model
+    y = y.view(-1, 1, 1) # This is the main function that runs the program
+
+    lstm_cell = nn.LSTMCell(1, num_units) # create the LSTM cell
+    output, (hn, cn) = lstm_cell(X, (torch.zeros(batch_size, num_units), torch.zeros(batch_size, num_units))) # run the LSTM cell on the input data
+
+    loss_fn = nn.CrossEntropyLoss() # create the loss function
+    optimizer = optim.Adam(lstm_cell.parameters(), lr=learning_rate) # create the optimizer
+
+    for epoch in range(epochs): # for each epoch
+        optimizer.zero_grad()
+        output, (hn, cn) = lstm_cell(X, (torch.zeros(batch_size, num_units), torch.zeros(batch_size, num_units))) # run the LSTM cell on the input data
+
+        loss = loss_fn(output, y) # calculate the loss
+        loss.backward() # backpropagate the loss
+        optimizer.step() # update the weights
+        print(f'Epoch: {epoch}, Loss: {loss.item()}')
+
+
+    #evaluate the model
+    with torch.no_grad(): # turn off gradient tracking
+        logits = lstm_cell(X, (torch.zeros(batch_size, num_units), torch.zeros(batch_size, num_units))) # run the LSTM cell on the input data
+        predictions = torch.argmax(logits, dim=1) # get the predictions
+        accuracy = (predictions == y).float().mean() # calculate the accuracy
+        print(f'Accuracy: {accuracy}') # print the accuracy
+
+
+def matts_cnn_model(train_data, train_labels):
+    """
+    CNN model that is trained on the data
+    Author: Matt
+    :param train_data:
+    :param train_labels:
+    :return:
+    """
+    print("-----------------------------\n"
+          "Matt's LSTM Model:\n")
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    train_data = scaler.fit_transform(train_data)
+
+
+
+
+
 
 
 def lucas_train(train, test):
